d061e80e818dd27d1f77cb2ccdde388c2e42b67a
QJanice <queenobiekwuye@gmail.com>
2022-09-08 19:11:14 -0700

    C program that prints exact statement followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: The program just prints text using puts
  *
- *   *
- *
- *    *Description: The program just prints text using puts
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
