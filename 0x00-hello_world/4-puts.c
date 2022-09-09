f8911d97069714fad898de2dbb4552e93a3e8458
QJanice <queenobiekwuye@gmail.com>
2022-09-08 17:05:59 -0700

    use the fxn put to print a file that prints a statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    *Description: The program just prints text using puts
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
